21addabfd711c5fd26b6305a5faf49264b1d6180
Aleh Balakhanau <aleh.balakhanau@solera.com>
2022-10-05 21:48:56 +0400

    Initial commit

---
+++ src/main/java/com/github/magicexists/checktelegrambot/CheckTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.magicexists.checktelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckTelegramBotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CheckTelegramBotApplication.class, args);
+  }
+
+}
